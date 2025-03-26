7dcbf280dd1cbc2c17094949e179c6e76bca7bec
humphrey de castro <106229467+poonstop@users.noreply.github.com>
2025-03-26 08:33:57 +0800

    NOVM on logout confirmation

--- templates/header.php
+++ templates/header.php
@@ -117,7 +117,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    Are you sure you want to log out of the NOV System?
+                    Are you sure you want to log out of the NOVM System?
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
