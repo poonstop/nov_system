7d059c75cc2ba9e247d594849bfc996115987202
humphrey de castro <106229467+poonstop@users.noreply.github.com>
2025-04-24 16:19:11 +0800

    main

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -33,12 +33,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
             </a>
         </li>
         <li>
-            <a href="establishments.php" class="<?php echo ($current_page == 'establishments.php') ? 'active' : ''; ?>">
-                <div class="icon-container">
-                    <i class="fas fa-exclamation-triangle"></i>
-                </div>
-                <span class="nav-text">Notice Management</span>
-            </a>
+        
         </li>
         <li>
             <a href="nov_form.php" class="<?php echo ($current_page == 'nov_form.php') ? 'active' : ''; ?>">
